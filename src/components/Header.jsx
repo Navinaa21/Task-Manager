6ab54387eb0f955d64774100b40c6665a11be768
Navinaa <navihema2112@gmail.com>
2023-07-18 22:03:52 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TaskAltIcon from '@mui/icons-material/TaskAlt';
+function Header() {
+  return (
+    <header>
+      <h1>
+        Notes  <TaskAltIcon />
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
